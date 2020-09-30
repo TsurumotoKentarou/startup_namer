e4c949253555ac2ef73cb358bedcf8da93e29042
tsurumoto <tsurumotokentarou@gmail.com>
2020-10-01 00:29:15 +0900

    english_wordsを使って起動毎に英単語を切り替えるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';  // Add this line.
 
 void main() {
   runApp(MyApp());
@@ -7,14 +8,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center(
+          //child: Text('Hello World'),   // Replace this text...
+          child: Text(wordPair.asPascalCase),  // With this text.
         ),
       ),
     );
